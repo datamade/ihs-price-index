280cc06f2a60401665ce87ef962c6295abf9321c
Derek Eder <derek.eder@gmail.com>
2015-05-05 09:44:30 -0500

    cleared out housing stock stuff, setup highcharts, added raw data

--- js/app.js
+++ js/app.js
@@ -1,102 +1,48 @@
-// logic for iframe view
-var is_iframe = false;
-if (window.location.href.indexOf("iframe=1") >= 0) {
-  is_iframe = true;
-  $("#topNav").hide();
-  $("#map").css("top", "0");
-}
-
-var tooltip_template = "<h3>{{{CCA_Census}}}</h3>\
-  <table class='tooltip'>\
-    <thead>\
-      <tr>\
-        <th>Housing Composition</th>\
-        <th>{{{REF_GEOG}}}</th>\
-        <th>Highlighted Area</th>\
-      </tr>\
-    </thead>\
-    <tbody>\
-      <tr>\
-        <td class='legend-box'><span style='background:#F5D4A3;'></span> Single-Family</td>\
-        <td>{{{Rshre_131}}}%</td>\
-        <td>{{{share_131}}}%</td>\
-      </tr>\
-      <tr>\
-        <td class='legend-box'><span style='background:#EDAB50;'></span> Condominium</td>\
-        <td>{{{Rshre_132}}}%</td>\
-        <td>{{{share_132}}}%</td>\
-      </tr>\
-      <tr>\
-        <td class='legend-box'><span style='background:#8DA7CE;'></span> 2-4 Unit Bldg</td>\
-        <td>{{{Rshre_133}}}%</td>\
-        <td>{{{share_133}}}%</td>\
-      </tr>\
-      <tr>\
-        <td class='legend-box'><span style='background:#375481;'></span> 5-49 Unit Bldg</td>\
-        <td>{{{Rshre_1341}}}%</td>\
-        <td>{{{share_1341}}}%</td>\
-      </tr>\
-      <tr>\
-        <td class='legend-box'><span style='background:#1F2F47;'></span> 50+ Unit Bldg</td>\
-        <td>{{{Rshre_1342}}}%</td>\
-        <td>{{{share_1342}}}%</td>\
-      </tr>\
-    </tbody>\
-  </table>\
-  <div class='legend-source'><a href='http://www.housingstudies.org/research-publications/publications/composition-cook-countys-housing-market/' target='_blank'>Read the report &raquo;</a><br /></div>\
-  <div class='legend-source'>Source: <a href='http://www.housingstudies.org/dataportal/info/composition/' target='_blank'>IHS calculations from the Cook County Assessor</a></div>\
-  </div>\
-";
-
-var legend_template = "<div id='my-legend'>\
-    <h4>Housing Composition</h4>\
-    <div class='legend-scale'>\
-      <ul class='legend-labels'>\
-        <li><span style='background:#F5D4A3;'></span>Single-Family</li>\
-        <li><span style='background:#EDAB50;'></span>Condominium</li>\
-        <li><span style='background:#8DA7CE;'></span>2-4 Unit Bldg</li>\
-        <li><span style='background:#375481;'></span>5-49 Unit Bldg</li>\
-        <li><span style='background:#1F2F47;'></span>50+ Unit Bldg</li>\
-      </ul>\
-    </div>";
-
-var mapbox_id = 'housingstudies.hace6l6h';
-
-var map = L.mapbox.map('map', '', {minZoom: 10, maxZoom:16, doubleClickZoom: false} ).setView([41.86943299643522, -87.72926330566406], 11);
-var hash = new L.Hash(map); //keep track of map location
-var gridLayer = L.mapbox.gridLayer(mapbox_id);
-map.addLayer(L.mapbox.tileLayer(mapbox_id));
-map.addLayer(gridLayer);
-
-if (is_iframe) {
-  var legend = L.mapbox.legendControl({position: 'bottomleft'});
-  legend.addLegend(legend_template);
-  map.addControl(legend);
-}
-else {
-  var tooltip = L.mapbox.gridControl(gridLayer, {position: 'bottomright', pinnable: false});
-  tooltip.setTemplate(tooltip_template);
-  map.addControl(tooltip);
-}
-
-// https://a.tiles.mapbox.com/v3/{mapbox_id}/markers.geojson
-var commAreas;
-$.when($.getJSON('data/cook-community-areas.geojson')).then(
-  function(communities){
-    commAreas = L.geoJson(communities, {
-      style: {opacity: 0, color: "#EDAB50", fillOpacity: 0},
-      onEachFeature: function(feature, layer){
-        layer.on('mouseover', function(e){
-          layer.setStyle({opacity: 0.7})
-        });
-        layer.on('mouseout', function(e){
-          layer.setStyle({opacity: 0})
-        });
-        layer.on('click', function(e){
-          map.fitBounds(e.target.getBounds(), {padding: [50,50]});
-        });
-      }
-    })
-    commAreas.addTo(map);
-  }
-)
+$(function () {
+    $('#chart').highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    });
+});
