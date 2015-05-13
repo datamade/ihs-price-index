f267775845ef5f59d2bfd5cece329150747bbe2d
Derek Eder <derek.eder@gmail.com>
2015-05-13 09:19:54 -0500

    setup analytics code

--- js/analytics_lib.js
+++ js/analytics_lib.js
@@ -10,7 +10,7 @@
  *
  */
 
-var analyticsTrackingCode = 'UA-XXXXXXXX-X'; //enter your tracking code here
+var analyticsTrackingCode = 'UA-62937118-1'; //enter your tracking code here
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', analyticsTrackingCode]);
