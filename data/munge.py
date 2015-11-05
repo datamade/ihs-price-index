645e8f32b1933963bcb0d6cecb21b21423f8d949
Eric van Zanten <eric.vanzanten@gmail.com>
2015-11-05 12:49:22 -0600

    Getting summary numbers The Right Way :tm:

--- data/munge.py
+++ data/munge.py
@@ -27,34 +27,45 @@ for row in rotated_rows:
             change = (float(cell) - float(row[baseline_index]))
             changeified_row.append(change)
     
-    peak_to_current = '%.1f' % (float(changeified_row[-1]) - float(max(changeified_row)))
-    peak_to_bottom = '%.1f' % (float(min(changeified_row)) - float(max(changeified_row)))
-    change_since_base = '%.1f' % (float(changeified_row[-1]) - float(changeified_row[baseline_index]))
-    bottom_to_current = '%.1f' % (float(changeified_row[-1]) - float(min(changeified_row)))
-
-    changeified_row.extend([peak_to_current, 
-                            peak_to_bottom, 
-                            change_since_base, 
-                            bottom_to_current])
-    
     changeified_row = ['%.1f' % float(i) for i in changeified_row]
 
     changes.append(changeified_row)
 
-changes_header = ['Change Peak to Current', 
-                  'Change Peak to Bottom', 
-                  'Change Since 2000',
-                  'Change Bottom to Current']
 
-header = ['PumaID', 'Name'] + quarters + changes_header
 rows_plus_names = []
 for index, row in enumerate(changes):
     row = [puma_ids[index], puma_names[index]] + row
     rows_plus_names.append(row)
     
+row_mapper = {}
+for row in rows_plus_names:
+    row_mapper[row[0]] = row
+
+rows_with_summary = []
+with open('2015_q2_data_for_web_final_summary.csv', 'r') as f:
+    reader = csv.reader(f)
+
+    next(reader)
+
+    for row in reader:
+        puma_id = row[0][1:].zfill(5)
+        try:
+            full_row = row_mapper[puma_id]
+            full_row.extend([i[:-1] for i in row[2:]])
+            rows_with_summary.append(full_row)
+        except KeyError:
+            break
+
+summary_header = ['Change Peak to Current', 
+                  'Change Peak to Bottom', 
+                  'Change Since 2000', 
+                  'Change Bottom to Current']
+
+header = ['PumaID', 'Name'] + quarters + summary_header
 
 with open('cook_puma_trend_by_quarter_q2.csv', 'w') as outp:
     writer = csv.writer(outp)
     writer.writerow(header)
-    writer.writerows(rows_plus_names)
+    writer.writerows(rows_with_summary)
+
 
