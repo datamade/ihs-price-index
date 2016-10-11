c45ffb1b1f207482bcb0c3c1aa8890085152972a
Derek Eder <derek.eder@gmail.com>
2016-10-11 17:11:10 -0500

    data update for 2016Q2

--- data/munge.py
+++ data/munge.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('2015_q4_data_for_web_final.csv', 'r') as f:
+with open('2016_q2_data_for_web_final.csv', 'r') as f:
     reader = csv.reader(f)
 
     puma_ids = next(reader)[1:]
@@ -42,7 +42,7 @@ for row in rows_plus_names:
     row_mapper[row[0]] = row
 
 rows_with_summary = []
-with open('2015_q4_data_for_web_final_summary.csv', 'r') as f:
+with open('2016_q2_data_for_web_final_summary.csv', 'r') as f:
     reader = csv.reader(f)
 
     next(reader)
