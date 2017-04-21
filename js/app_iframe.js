fe19c115b832e9b7c40cdf8510f050fb2dbe56ba
Derek Eder <derek.eder@gmail.com>
2017-04-21 14:39:56 -0500

    update for q4 2016

--- js/app_iframe.js
+++ js/app_iframe.js
@@ -43,7 +43,7 @@ function init_chart(){
             renderTo: 'chart'
         },
         title: {
-            text: "Cook County House Price Index: Jan 2000 - June 2016",
+            text: "Cook County House Price Index: Jan 2000 - Dec 2016",
             x: -20 //center
         },
         credits: { enabled: false },
